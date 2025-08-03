82d78af9abc155c008dc311c60a84ea1b248b1e0
pr0fix <bgx152@myy.haaga-helia.fi>
2025-08-03 19:34:03 +0300

    feat: add cat service for fetching daily cats; implement API call and error handling

---
+++ client/src/services/cat.ts
@@ -0,0 +1,28 @@
+import axios from "axios";
+import { CAT_BASE_URL } from "../utils/constants";
+import auth from "./auth";
+
+const getDailyCats = async (userId: string) => {
+  try {
+    const userToken = auth.getToken();
+    const res = await axios.get(`${CAT_BASE_URL}/daily`, {
+      headers: { Authorization: `Bearer ${userToken}` },
+    });
+    console.log(res.data);
+    return res.data;
+  } catch (error) {
+    console.error("Error fetching daily cats");
+    if (axios.isAxiosError(error)) {
+      const message =
+        error.response?.data?.message || "Failed to fetch daily cats";
+      console.error(`API Error: ${message}`);
+      return { error: true, message, status: error.response?.status };
+    }
+    console.error("Unexpected error:", error);
+    return {
+      error: true,
+      message: "An unexpected error occurred",
+      originalError: error,
+    };
+  }
+};
--- client/src/utils/constants.ts
+++ client/src/utils/constants.ts
@@ -1,1 +1,2 @@
 export const AUTH_BASE_URL = "http://localhost:3000/api/auth";
+export const CAT_BASE_URL = "http://localhost:3000/api/cats";
